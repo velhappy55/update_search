753b7e691e7e6af34c86d73ce3433945e0f562fa
velhappy55 <velhappy55@gmail.com>
2020-06-15 03:30:06 +0530

    Updates

---
+++ precache-manifest.b2492a1edc29d2fa8c745faef1afd489.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8816a5efd87764d15c0a0ecf0807d2ab",
+    "url": "/update_search/index.html"
+  },
+  {
+    "revision": "6145d0ec5e44bba5c7d9",
+    "url": "/update_search/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "71250969895c4f44074b",
+    "url": "/update_search/static/css/main.bf1f6035.chunk.css"
+  },
+  {
+    "revision": "6145d0ec5e44bba5c7d9",
+    "url": "/update_search/static/js/2.ba72bdc8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/update_search/static/js/2.ba72bdc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71250969895c4f44074b",
+    "url": "/update_search/static/js/main.aaa521d5.chunk.js"
+  },
+  {
+    "revision": "d97daf27b229ed44c47b",
+    "url": "/update_search/static/js/runtime-main.409af724.js"
+  },
+  {
+    "revision": "1d45214cd72a89bec8b2b9c0c9f3418e",
+    "url": "/update_search/static/media/red3.1d45214c.jpg"
+  }
+]);
